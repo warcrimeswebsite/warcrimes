8060003faacc6f98bb959172a34c3c8e8b4d3b39
warcrimeswebsite <iiggzzss@proton.me>
2025-04-26 20:41:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleContent() {
+    let content = document.getElementById("content");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
